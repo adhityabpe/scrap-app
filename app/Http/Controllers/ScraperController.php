bb80340bc9099e668f542b535c6a30de27dd6f6d
adhit <adhitya@triyakom.com>
2023-11-30 11:34:03 +0700

    first commit

---
+++ app/Http/Controllers/ScraperController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Goutte\Client;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpClient\HttpClient;
+use Illuminate\Support\Facades\Storage; 
+use Illuminate\Filesystem\Filesystem;   
+
+class ScraperController extends Controller
+{
+    function dateKurs($date)
+    {
+        $date = str_replace('Kurs Dollar Hari Ini - ', '', $date);
+        // remove whitespace &nbsp
+        $dateArray = explode(",\xC2\xA0", $date);
+
+        return $dateArray;
+    }
+
+    function word($text)
+    {
+        $text = str_replace(
+            array('<br>', '<nobr>', '</nobr>'),
+            array('', ' ', ''),
+            $text,
+        );
+        return $text;
+    }
+
+    public function jsonDollar()
+    {
+
+        $client = new Client(HttpClient::create(['timeout' => 60]));
+
+        $url = 'https://kursdollar.org/';
+
+        $crawler = $client->request('GET', $url);
+
+        $table = $crawler->filter('table.in_table')->first();
+
+        $date = $table->filter('.in_table tr:not([id]):not([class]) td')->text();
+
+        $meta = $table->filter('.in_table tr:nth-child(2)')->each(function ($row) use ($date) {
+            return [
+                'date' => $this->dateKurs($date)[1],
+                'day' => $this->dateKurs($date)[0],
+                'indonesia' => $row->filter('td:nth-child(2)')->text(),
+                'word' => $this->word($row->filter('td:nth-child(3)')->html()),
+
+            ];
+        });
+
+        $rates = $table->filter('.in_table tr[id]')->each(function ($row) {
+            return [
+                'currency' => $row->filter('td a strong')->text(),
+                'buy' => $row->filter('td')->eq(1)->text(),
+                'sell' => $row->filter('td')->eq(2)->text(),
+                'average' => $row->filter('td')->eq(3)->text(),
+                'word_rate' => $row->filter('td')->eq(4)->text(),
+
+            ];
+        });
+        date_default_timezone_set('Asia/Jakarta');
+        $fileNames='rate-'.date('d-m-Y-h-i-a').'.json'; 
+        $file = new Filesystem;
+        $file->cleanDirectory('storage/app/backgrounds');
+
+        Storage::disk('public')->put($fileNames,json_encode(['meta' => $meta, 'rates' => $rates]));
+
+
+        return response()->json(['meta' => $meta, 'rates' => $rates]);
+    }
+}
---
+++ resources/views/scraper.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kurs Scrap</title>
+    <style>
+        .myButton {
+	background-color:#44c767;
+	border-radius:28px;
+	border:1px solid #18ab29;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:17px;
+	padding:16px 31px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #2f6627;
+}
+.myButton:hover {
+	background-color:#5cbf2a;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+    </style>
+</head>
+<body>
+    <a href="/api/kurs" class="myButton">Scrap</a>
+</body>
+</html>
